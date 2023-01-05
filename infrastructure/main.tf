92bce77ddb209c5e94c0e7855de5ddc1635dcbf5
NicoleSelig <nlselig@sep.com>
2023-01-05 07:41:57 -0500

    initiate backend in github action instead of terra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,25 @@
-data "external" "env" {
-  program = ["${path.module}/env.sh"]
-}
-
 provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "dev-lol-scout-rg"
-    storage_account_name = "devlolscoutsa01"
+data "terraform_remote_state" "state" {
+  backend = "azurerm"
+  config {
+    resource_group_name  = "${var.env}-lol-scout-rg"
+    storage_account_name = "${var.env}lolscoutsa01"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "dev-lol-scout-rg"
+#     storage_account_name = "devlolscoutsa01"
+#     container_name       = "terraform-state"
+#     key                  = "terraform.tfstate"
+#   }
+# }
 
 resource "azurerm_resource_group" "lol_scout_rg" {
   name     = "dev-lol-scout-rg"
